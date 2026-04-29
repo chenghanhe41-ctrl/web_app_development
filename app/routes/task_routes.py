168c3ff3f9a0f8c5bb26124ae89622dd6e347043
chenghanhe41 <chenghanhe41@gmail.com>
2026-04-29 17:57:15 +0800

    feat: add route skeleton and template plan

---
+++ app/routes/task_routes.py
@@ -0,0 +1,27 @@
+﻿from flask import Blueprint, render_template, request, redirect, url_for
+
+task_bp = Blueprint('tasks', __name__)
+
+@task_bp.route('/')
+def index():
+    return render_template('index.html')
+
+@task_bp.route('/tasks/new', methods=['GET', 'POST'])
+def create_task():
+    if request.method == 'POST':
+        return redirect(url_for('tasks.index'))
+    return render_template('create_task.html')
+
+@task_bp.route('/tasks/<int:task_id>/edit', methods=['GET', 'POST'])
+def edit_task(task_id):
+    if request.method == 'POST':
+        return redirect(url_for('tasks.index'))
+    return render_template('edit_task.html')
+
+@task_bp.route('/tasks/<int:task_id>/delete', methods=['POST'])
+def delete_task(task_id):
+    return redirect(url_for('tasks.index'))
+
+@task_bp.route('/tasks/<int:task_id>/toggle', methods=['POST'])
+def toggle_task(task_id):
+    return redirect(url_for('tasks.index'))
